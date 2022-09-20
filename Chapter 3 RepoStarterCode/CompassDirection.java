3ad6f0b410c1794cc4bf486f374faeb68c8d204b
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2022-09-20 13:36:30 -0500

    added some programs

--- Chapter 3 RepoStarterCode/CompassDirection.java
+++ Chapter 3 RepoStarterCode/CompassDirection.java
@@ -9,16 +9,30 @@ public class CompassDirection
    {
       Scanner in = new Scanner(System.in);
       System.out.print("Enter the direction the compass is pointing " +
-         + " (in degrees from North (0..360): ");
+         " (in degrees from North (0..360): ");
       double degrees = in.nextDouble();
 
       System.out.print(degrees + " Degrees is ");
-
+      
       // Calculate the direction and print the rest
       // of the line here
-
-
-
-
+      // strings don't work because they compare memory addresses 
+      
+      if (degrees < 45 || degrees == 360)
+          System.out.print("North");
+      else if (degrees < 90)
+          System.out.print("Northeast");
+      else if (degrees < 135)
+          System.out.print("East");
+      else if (degrees < 180)
+          System.out.print("Southeast");
+      else if (degrees < 225)
+          System.out.print("South");
+      else if (degrees < 270)
+          System.out.print("Southwest");
+      else if (degrees < 315)
+          System.out.print("West");
+      else
+          System.out.print("Northwest");
    }
 }
