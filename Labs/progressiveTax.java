9a7f53de670d18acac83a488ba7b5be7d4332b7e
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2022-09-26 14:10:31 -0500

    created tax program

--- Labs/progressiveTax.java
+++ Labs/progressiveTax.java
@@ -11,9 +11,26 @@ public class progressiveTax
         Scanner scanner = new Scanner(System.in);
         
         double preTax = scanner.nextDouble();
+        double taxAmount = 0;
         
         double incomeLevel = preTax / 50000;
-        
-        if(incomeLevel =< 1.5 
+        if( 50000>= preTax){
+            System.out.println("No Tax!");
+        }else if( preTax >50000 && 75000>= preTax){
+            taxAmount = preTax * .02;
+            System.out.println("Tax Amount: " + taxAmount);
+        }else if( preTax >75000 && 100000>= preTax){
+            taxAmount = preTax * .03;
+            System.out.println("Tax Amount: " + taxAmount);
+        }else if( preTax >100000 && 250000>= preTax){
+            taxAmount = preTax * .04;
+            System.out.println("Tax Amount: " + taxAmount);
+        }else if( preTax >250000 && 500000>= preTax){
+            taxAmount = preTax * .05;
+            System.out.println("Tax Amount: " + taxAmount);
+        }else if( preTax >500000){
+            taxAmount = preTax * .06;
+            System.out.println("Tax Amount: " + taxAmount);
+        }
     }
 }
