fcda73180eecd8263a1cece65a4084f4c5390a75
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2022-09-27 14:11:30 -0500

    setting up graphics

---
+++ Chapter 3 RepoStarterCode/Labs Graphics/drawExample.java
@@ -0,0 +1,16 @@
+import TurtleGraphics.StandardPen;
+import java.awt.*;
+/**
+ * Write a description of class drawExample here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class drawExample
+{
+    public static void main(String[] args){
+        
+        StandardPen pen = new StandardPen();
+        
+    }
+}
---
+++ Chapter 3 RepoStarterCode/Labs/triangleCheck.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class triangleCheck here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class triangleCheck
+{
+    public static void main(String[] args){
+        
+    }
+}
