59d8d11d7ca923b24ef83265b05ddff554dc4def
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2022-09-28 14:20:50 -0500

    create planet switch

--- Chapter 3 RepoStarterCode/Labs/solarSystem.java
+++ Chapter 3 RepoStarterCode/Labs/solarSystem.java
@@ -10,6 +10,19 @@ import java.util.*;
 public class solarSystem
 {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(Scanner.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How much do you weigh:");
+        double weight = scanner.nextDouble();
+        double mass = weight/9.81;
+        
+        System.out.println("Choose a Planet:\nMer -Mercury\nVen -Venus\nMo -Moon\nEa -Earth\nMa - Mars\nJup -Jupiter\nSat - Saturn\nNep - Neptune\n Ur - Uranus");
+        String choice = scanner.nextLine();
+        
+        
+        switch(choice){
+            case(choice.equals("Mer")):
+                
+            }
+        }
     }
 }
