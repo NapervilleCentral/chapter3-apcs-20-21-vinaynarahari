99da4bd92636c2f08f9f5a6d5b94acb113af815a
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2022-10-05 14:23:21 -0500

    made changes

--- Chapter3B Starter Code/e02/PowerOf2.java
+++ Chapter3B Starter Code/e02/PowerOf2.java
@@ -1,9 +1,18 @@
+package e02;
+
 public class PowerOf2
 {
    public static void main(String[] args)
    {
       // The powers of 2 from 2^0 to 2^20
       //display and sum all of the squares 1, 2 , 4 , 8 ...
-
+      int sum = 0;
+      
+      
+      for( int x  = 0; 20>=x; x++){
+          sum += Math.pow(2,x);
+          System.out.println(Math.pow(2,x));
+      }
+      System.out.println("Sum is: " + sum);
    }
 }
