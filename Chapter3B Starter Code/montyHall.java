2a87113a204c6d6fbf5c2008a784365cbd09a080
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2022-10-18 14:19:21 -0500

    changes

--- Chapter3B Starter Code/montyHall.java
+++ Chapter3B Starter Code/montyHall.java
@@ -11,22 +11,34 @@ public class montyHall
         
         Random random = new Random();
   
-        int h  = 0 , g = 0 , j =0;
-        
+        int h  = 0 , g = 0 , j =0; //creating arbritrary values to add later
+        // if they choose to win 
         for(int x = 0; 1000>=x ; x++){
             int y = random.nextInt(3) + 1;  // user
             int c = random.nextInt(3) + 1;  // right door
             
             if(y == c){
                 g++;
-            }else if ( y==2){
+            }else 
                 h++;
-            }else if( y ==3){
-                j++;
-            }
             
         }
-        System.out.println(g);
-        System.out.println(h+j);
+        System.out.println("Win "+ h);
+        System.out.println("loss " + g);
+        
+        g =0;
+        h =0;
+        // if they choose to not switch
+        for( int i = 0; 1000>=i; i++){
+            int y = random.nextInt(3) + 1;  // user
+            int c = random.nextInt(3) + 1;  // right door
+            if( y ==c){
+                h++;
+            }else
+                g++;
+        }
+        System.out.println("Win "+ h);
+        System.out.println("loss " + g);
+        
     }
 }
---
+++ Chapter3B Starter Code/triangleofstars.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class triangleofstars here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class triangleofstars
+{
+    public static void main(String[] args){
+        
+    }
+}
