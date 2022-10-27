54960eda8af36b13eb20681e0eeb4a5cc14a74c8
Vinay Narahari <111399243+vinaynarahari@users.noreply.github.com>
2022-10-27 00:40:15 -0500

    changes

--- Chapter3B Starter Code/parksides.java
+++ Chapter3B Starter Code/parksides.java
@@ -8,18 +8,54 @@ import java.util.*;
 public class parksides
 {
     public static void main(String[] args){
+        System.out.println();
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Enter the size: ");
         int rows = scanner.nextInt();
         System.out.println("Enter the Seed: ");
-        int start = scanner.nextInt();
+        Scanner scanner1 = new Scanner(System.in);
+        int start = scanner1.nextInt();
+        int holder = start;
+        String empty = "";
         
-        for(int x = 1; rows>=x; x++){
+        for(int x =rows, y =0;x >= 0;x--, y++){
             
+            holder = holder + y;
+            if (holder>9){
+                holder = start; 
+                
+            }
+            System.out.print(holder + " ");
             
         }
         
-        
+        for(int k =rows-1, i =0 ; k!=0; k--){
+            System.out.println();
+            while(k<rows){
+                System.out.print(" ");
+                int z = k;
+                z++;
+            }
+            holder = holder + i;
+            if (holder>9){
+                holder = start; 
+                
+            }
+            System.out.print(holder + " ");
+        }
+        /*
+           holder = holder + y;
+            if (holder>9){
+                holder = start; 
+                
+            }
+            System.out.print(holder + " ");
+            newrow = newrow-z;
+            while(newrow<rows){
+                empty.concat(" ");
+                x=1;
+            }
+         */ 
     }
 }
